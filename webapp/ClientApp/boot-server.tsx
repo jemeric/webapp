631ad29232cf0adddf1bc35f963402c41c7ba5a5
jemeric <jericmason@gmail.com>
2019-01-14 22:57:16 -0800

    Refactoring to move all into server-side react, so styles can be rendered in <head>

--- webapp/ClientApp/boot-server.tsx
+++ webapp/ClientApp/boot-server.tsx
@@ -10,14 +10,19 @@ export default createServerRenderer(params => {
         // this context object contains the results of the render (i.e. context.url will contain URL to redirect to if <Redirect> was used)
         const routerContext: any = {};
 
+        const urls = params.data as string[];
+
         // see styled components server-side rendering - https://www.styled-components.com/docs/advanced#server-side-rendering
         const sheet = new ServerStyleSheet();
 
         // static router = a router that never changes location (on server-side render things won't change)
+        var scriptUrls = urls.concat(",");
         const app = (
-            <StaticRouter context={routerContext} location={params.location.path}>
-                <App compiler="Typescript" framework="React" />
-            </StaticRouter>
+            <div id="example">
+                <StaticRouter context={routerContext} location={params.location.path}>
+                    <App compiler="Typescript" framework="React" />
+                </StaticRouter>
+            </div>
         );
 
         // If there's a redirection, just send this information back to the host application
@@ -26,7 +31,9 @@ export default createServerRenderer(params => {
             return;
         }
 
-        const html = renderToString(sheet.collectStyles(app));
+        sheet.collectStyles(app);
+
+        const html = renderToString(app);
 
         // TODO - we need to get this into the document head
         const styleTags = sheet.getStyleTags();
@@ -34,7 +41,7 @@ export default createServerRenderer(params => {
         // once any async tasks are done, we can perform the final render
         params.domainTasks.then(() => {
             resolve({
-                html: styleTags + html
+                html: styleTags + html + scriptUrls // you can also use this to initialize globals from the server-side
             });
         }, reject); // also propagate any errors back into the host application
     });
