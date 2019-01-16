b24d4b1cb0f6db7a3cb7b037cb4a737576d1c4c1
jemeric <jericmason@gmail.com>
2019-01-15 21:55:19 -0800

    Moved outer template into typescript server rendering so style tags can be included correctly

--- webapp/ClientApp/boot-server.tsx
+++ webapp/ClientApp/boot-server.tsx
@@ -5,24 +5,37 @@ import { StaticRouter } from 'react-router-dom';
 import { App } from './components/App';
 import { ServerStyleSheet } from 'styled-components';
 
+
+function getTemplate(app: string, styles: string, scripts: string[]) : string {
+    return `<head>
+                <meta charset="UTF-8" />
+                <title>Hello React!</title>
+                <link rel="stylesheet" type="text/css" href="/dist/style.css">
+                ${styles}
+            </head>
+            <body>
+                <div id="example">${app}</div>
+                ${scripts.map(url => `<script src="${url}"></script>`).join('')}
+                <!-- Main -->
+                <script src="/dist/main-client.js"></script>
+            </body>`;
+}
+
 export default createServerRenderer(params => {
     return new Promise<RenderResult>((resolve, reject) => {
         // this context object contains the results of the render (i.e. context.url will contain URL to redirect to if <Redirect> was used)
         const routerContext: any = {};
 
-        const urls = params.data as string[];
+        const scripts = params.data as string[];
 
         // see styled components server-side rendering - https://www.styled-components.com/docs/advanced#server-side-rendering
         const sheet = new ServerStyleSheet();
 
         // static router = a router that never changes location (on server-side render things won't change)
-        var scriptUrls = urls.concat(",");
         const app = (
-            <div id="example">
-                <StaticRouter context={routerContext} location={params.location.path}>
-                    <App compiler="Typescript" framework="React" />
-                </StaticRouter>
-            </div>
+            <StaticRouter context={routerContext} location={params.location.path}>
+                <App compiler="Typescript" framework="React" />
+            </StaticRouter>
         );
 
         // If there's a redirection, just send this information back to the host application
@@ -31,9 +44,7 @@ export default createServerRenderer(params => {
             return;
         }
 
-        sheet.collectStyles(app);
-
-        const html = renderToString(app);
+        const html = renderToString(sheet.collectStyles(app));
 
         // TODO - we need to get this into the document head
         const styleTags = sheet.getStyleTags();
@@ -41,7 +52,7 @@ export default createServerRenderer(params => {
         // once any async tasks are done, we can perform the final render
         params.domainTasks.then(() => {
             resolve({
-                html: styleTags + html + scriptUrls // you can also use this to initialize globals from the server-side
+                html: getTemplate(html, styleTags, scripts) // you can also use this to initialize globals from the server-side
             });
         }, reject); // also propagate any errors back into the host application
     });
