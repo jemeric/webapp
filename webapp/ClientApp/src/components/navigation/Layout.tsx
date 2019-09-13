487b8d09659abdbd616fac8e46065fde4c97d825
jemeric <jericmason@gmail.com>
2019-09-12 23:11:08 -0700

    Migrating layout to styled components

--- webapp/ClientApp/src/components/navigation/Layout.tsx
+++ webapp/ClientApp/src/components/navigation/Layout.tsx
@@ -1,28 +1,52 @@
 import * as React from "react";
 import { Header } from "./Header";
 import { useScrollYPosition } from "react-use-scroll-position";
+import styled from "styled-components";
 
-const resetScroll = (isMobileNavOpen: boolean, contentScrollPosition: number) => {
-  if(isMobileNavOpen) {
+const resetScroll = (
+  isMobileNavOpen: boolean,
+  contentScrollPosition: number
+) => {
+  if (isMobileNavOpen) {
     window.scrollTo(0, 0);
   } else {
     window.scrollTo(0, contentScrollPosition);
   }
+};
+
+interface ILayoutContentProps {
+  contentTop: number;
 }
+
+const LayoutContent = styled("main")<ILayoutContentProps>`
+  // Page content
+  max-width: $content-width;
+  margin: 0 auto;
+  margin-top: 80px;
+  @media only screen and (max-width: 798px) {
+    margin-top: 0px;
+    &.mobileNavOpened {
+      top: ${props => props.contentTop}px;
+      position: fixed;
+      right: 400px;
+      width: 100vh;
+    }
+  }
+`;
 
 export function Layout(props: React.PropsWithChildren<{}>) {
   // const layoutRef = React.useRef<HTMLElement>(null);
   const [isMobileNavOpen, setIsMobileNavOpen] = React.useState(false);
   const [contentScrollPosition, setContentScrollPosition] = React.useState(0);
-  const onMobileToggle = (e: boolean) => { 
-    if(e !== isMobileNavOpen) setIsMobileNavOpen(e);
+  const onMobileToggle = (e: boolean) => {
+    if (e !== isMobileNavOpen) setIsMobileNavOpen(e);
   };
-  
+
   const scrollY = useScrollYPosition();
-  const contentScrollOffset = isMobileNavOpen ? { top: 70 - contentScrollPosition } : {};
+  const contentTop = 70 - contentScrollPosition;
   const mobileNavContentClass = isMobileNavOpen ? "mobileNavOpened" : "";
   React.useEffect(() => {
-    if(!isMobileNavOpen) {
+    if (!isMobileNavOpen) {
       // store the most recent scroll state when the mobile nav was closed
       setContentScrollPosition(scrollY);
     }
@@ -36,9 +60,9 @@ export function Layout(props: React.PropsWithChildren<{}>) {
   return (
     <div>
       <Header mobileNavToggleHandler={onMobileToggle} />
-      <main style={contentScrollOffset} className={mobileNavContentClass}>
+      <LayoutContent contentTop={contentTop} className={mobileNavContentClass}>
         {props.children}
-      </main>
+      </LayoutContent>
     </div>
   );
 }
